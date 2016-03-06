99b8d6b8a59c05a3a52bfc9b0514ed15fc852ddf
richytruitt <richytruitt@10.254.1.173>
2016-03-05 21:01:04 -0500

    Modified on Mar-5-2015 at 9:00pm

--- Assignment1/src/Server.java
+++ Assignment1/src/Server.java
@@ -1,9 +1,35 @@
-import java.net.Socket;
-public class Server 
-{
-	private static final int _port = 80;
-	public static void main(String[] args) 
-	{
+import java.net.*;
+import java.io.*;
+import java.util.Scanner; 
+
+public class Server {
+	
+
+	public static void main(String[] args) throws IOException{
+		int c1, c2, c3; 
 		
+		ServerSocket s1=new ServerSocket(21252); 
+		Socket ss=s1.accept();
+		//Scanner input = new Scanner(ss.getInputStream()); 
+		BufferedReader in=new BufferedReader(new InputStreamReader(ss.getInputStream()));
+		
+		
+		c1=in.read();
+		c2=in.read();  
+		c3=in.read();			
+		in.close();
+		
+		//PrintStream p=new PrintStream(ss.getOutputStream()); 
+		System.out.println("Port connected to is"+ss.getPort());
+		System.out.println("C.1= "+c1);
+		System.out.println(); 
+		System.out.println("C.2= "+c2);
+		System.out.println(); 
+		System.out.println("C.3= "+c3);
+			
+		s1.close();
+			
+		
+			 
 	}
 }
