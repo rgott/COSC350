011c1de1d8d700bf5511b0bbcdf0005409ecc89d
Rob Gott <robgott@yahoo.com>
2016-03-05 23:02:54 -0500

    Server Updates

--- Assignment1/src/Server.java
+++ Assignment1/src/Server.java
@@ -1,35 +1,44 @@
-import java.net.*;
-import java.io.*;
-import java.util.Scanner; 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket; 
 
 public class Server {
 	
-
+	private static final int _PORT = 21252;
 	public static void main(String[] args) throws IOException{
-		int c1, c2, c3; 
+		int c2, c3, c4; 
 		
-		ServerSocket s1=new ServerSocket(21252); 
-		Socket ss=s1.accept();
-		////Scanner input = new Scanner(ss.getInputStream()); 
-		BufferedReader in=new BufferedReader(new InputStreamReader(ss.getInputStream()));
+		ServerSocket serverSocket =new ServerSocket(_PORT); 
+		Socket clientConnection = serverSocket.accept();
+		//Scanner input = new Scanner(ss.getInputStream()); 
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
 		
 		
-		c1=in.read();
-		c2=in.read();  
-		c3=in.read();			
-		in.close();
+		try // try to parse the integer recieved as string
+		{
+			c2 = Integer.parseInt(inFromClient.readLine().split("=")[1]);
+			c3 = Integer.parseInt(inFromClient.readLine().split("=")[1]);  
+			c4 = Integer.parseInt(inFromClient.readLine().split("=")[1]);			
 		
-		//PrintStream p=new PrintStream(ss.getOutputStream()); 
-		System.out.println("Port connected to is"+ss.getPort());
-		System.out.println("C.1= "+c1);
-		System.out.println(); 
-		System.out.println("C.2= "+c2);
-		System.out.println(); 
-		System.out.println("C.3= "+c3);
+			System.out.println("Port connected to is: " + _PORT);
+			System.out.println("c.2=" + c2);
+			System.out.println(); 
+			System.out.println("c.3=" + c3);
+			System.out.println(); 
+			System.out.println("c.4=" + c4);
 			
-		s1.close();
-			
-		
-			 
+		}catch(NumberFormatException e)
+		{
+			System.out.println("Error: one or more of the inputs recieved has been corrupted or not an integer.");
+			e.printStackTrace();
+		}catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Error: bad input from client");
+			e.printStackTrace();
+		}
+		inFromClient.close();
+		serverSocket.close();
 	}
 }
